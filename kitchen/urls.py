7e25ad80cbcf587df58eff0d582b8f89159cdd85
David Gluschenko <davidglusch@gmail.com>
2023-03-28 18:47:50 +0300

    Added list and detail urls for all models

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,26 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import (
+    index,
+    # DishTypeDetailView,
+    DishTypeListView,
+    DishDetailView,
+    DishListView,
+    CookDetailView,
+    CookListView,
+
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-type/", DishTypeListView.as_view(), name="dish-type-list"),
+    # path("dish-type/<int:pk>", DishTypeDetailView.as_view(), name="dish-type-detail"),
+
+    path("dish/", DishListView.as_view(), name="dish-list"),
+    path("dish/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+
+    path("cook/", CookListView.as_view(), name="cook-list"),
+    path("cook/<int:pk>", CookDetailView.as_view(), name="cook-detail"),
 
 ]
 
