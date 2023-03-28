858aa245a3c370c6b5a3784a9d25d592443e4464
David Gluschenko <davidglusch@gmail.com>
2023-03-28 18:52:30 +0300

    Added all models to admin panel

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from kitchen.models import Dish, DishType, Cook
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
