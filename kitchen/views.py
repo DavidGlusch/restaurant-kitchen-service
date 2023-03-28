611e9b7ec421daded24c6c15a432a868c3814972
David Gluschenko <davidglusch@gmail.com>
2023-03-28 18:54:52 +0300

    Added list views for all models, detail views for Cook and Dish models

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Dish, DishType, Cook
 
@@ -7,11 +8,37 @@ def index(request):
     num_dishes = Dish.objects.count()
     num_dish_types = DishType.objects.count()
     num_cooks = Cook.objects.count()
+    num_visits = request.session.get("num_visits", 1)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_dishes": num_dishes,
         "num_dish_types": num_dish_types,
-        "num_cooks": num_cooks
+        "num_cooks": num_cooks,
+        "num_visits": num_visits,
     }
 
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type_list"
+
+
+class DishListView(generic.ListView):
+    model = Dish
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
+
+
+class CookDetailView(generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.prefetch_related("dishes__dish_type")
