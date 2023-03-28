7e6535ab0c2b29113ff2b1d1d66a389493d939bd
David Gluschenko <davidglusch@gmail.com>
2023-03-28 13:55:49 +0300

    Add Dish model to models with class Meta

--- kitchen/models.py
+++ kitchen/models.py
@@ -6,3 +6,18 @@ class DishType(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2)
+    dish_type = models.ForeignKey(
+        to=DishType,
+        on_delete=models.SET_NULL,
+        related_name="dishes",
+    )
+
+    class Meta:
+        verbose_name_plural = "dishes"
+        ordering = ["name"]
