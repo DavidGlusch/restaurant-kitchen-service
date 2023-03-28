7005fc65ccc5bf4ab2b013a9eaac9254e6981c0f
David Gluschenko <davidglusch@gmail.com>
2023-03-28 18:57:08 +0300

    Added static path into urls

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from kitchen.views import (
     index,
-    # DishTypeDetailView,
+
     DishTypeListView,
     DishDetailView,
     DishListView,
@@ -14,7 +14,7 @@ from kitchen.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("dish-type/", DishTypeListView.as_view(), name="dish-type-list"),
-    # path("dish-type/<int:pk>", DishTypeDetailView.as_view(), name="dish-type-detail"),
+
 
     path("dish/", DishListView.as_view(), name="dish-list"),
     path("dish/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
