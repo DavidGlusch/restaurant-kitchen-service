24b88c971011114dab2a948ba00eceaa4189b766
David Gluschenko <davidglusch@gmail.com>
2023-03-29 18:32:58 +0300

    Tweaked Cook model

--- kitchen/models.py
+++ kitchen/models.py
@@ -36,11 +36,14 @@ class Dish(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
-    REQUIRED_FIELDS = ["years_of_experience"]
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
 
     def get_absolute_url(self):
-        return reverse("taxi:cook-detail", kwargs={"pk": self.pk})
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
 
     def __str__(self):
-        return str(self.username)
+        return f"{self.username} ({self.first_name} {self.last_name})"
